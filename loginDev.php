045ca1755677206da15cdede76950e0df661494b
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-08 16:21:17 -0300

    Update loginDev.php

--- loginDev.php
+++ loginDev.php
@@ -14,7 +14,7 @@
       <script src="https://cdn.tailwindcss.com"></script>
   </head>
   <body class="w-screen h-screen flex flex-col items-center justify-center bg-[var(--off-white)]">
-        <main class="max-w-xs md:max-w-md flex flex-col gap-4 items-center p-4 md:p-8 bg-white rounded-md shadow-md">
+        <main class="max-w-xs md:max-w-md flex flex-col gap-4 items-center pb-2 md:pb-2 p-4 md:p-8 bg-white rounded-md shadow-md">
             <header class="flex gap-2 items-center">
                 <h1 class="text-2xl md:text-4xl font-semibold text-primary">Biblio<span class="text-secondary">etec</span></h1>
                 <span class="w-[2px] h-6 md:h-8  bg-secondary"></span>
@@ -35,7 +35,7 @@
 
                 <div class="input-group min-w-full flex flex-col gap-1">
                     <input type="submit" name="conectar" value="Entrar" class="w-full h-12 rounded shadow-sm bg-secondary text-2xl text-white font-semibold cursor-pointer">
-                    <button id="primeiroAcessoBtn" class="text-[var(--secondary-emphasis)] text-sm underline" data-modal-target="modal-primeiro-acesso" data-modal-toggle="modal-primeiro-acesso">Primeiro Acesso</button>
+                    <button id="primeiroAcessoBtn" class="mt-2 mb-0 text-[var(--secondary-emphasis)] text-sm underline" data-modal-target="modal-primeiro-acesso" data-modal-toggle="modal-primeiro-acesso">Primeiro Acesso</button>
                 </div>
             </form>
             <p>
@@ -49,7 +49,7 @@
         </main>
 
         <div id="modal-primeiro-acesso" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="bg-white rounded-md shadow-md min-w-80 px-8 py-4 flex flex-col gap-8">
+            <div class="bg-white rounded-md shadow-md min-w-80 px-8 pb-8 pt-4 flex flex-col gap-8">
                 <div class="flex flex-col">
                     <button class="w-full text-center text-xs text-[var(--primary-emphasis)] underline" data-modal-hide="modal-primeiro-acesso">Sair</button>
                     <h2 class="text-primary font-semibold text-2xl text-center text-nowrap">Valide o acesso</h2>
